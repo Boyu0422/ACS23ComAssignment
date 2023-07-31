0ff95944bd4402d528b81009e1ec4c9a522c553c
Boyu345 <li000769@algonquinlive.com>
2023-07-30 20:40:41 -0400

    Initial A31

--- A22/codeA22/out/build/x64-Debug/cmake_install.cmake
+++ A22/codeA22/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/15179/Downloads/A22/codeA22
+# Install script for directory: C:/Course Assignment/Complier/Boyu345/A22/codeA22
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/15179/Downloads/A22/codeA22/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Course Assignment/Complier/Boyu345/A22/codeA22/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/15179/Downloads/A22/codeA22/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Course Assignment/Complier/Boyu345/A22/codeA22/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
